54d5acc4bee21f1820f2672cbdc68160489d19cd
Richard Lehane <richard.lehane@gmail.com>
2025-07-14 13:48:20 +1000

    Update authority.dart

--- lib/src/authority.dart
+++ lib/src/authority.dart
@@ -1,6 +1,10 @@
-import 'package:xml/xml.dart';
+import 'authority_none.dart' // Stub implementation
+    if (dart.library.io) 'authority_win.dart' // dart:io implementation
+    if (dart.library.js_interop) 'authority_web.dart'; // package:web implementation
+import 'package:xml/xml.dart' show XmlElement;
+import 'package:fluent_ui/fluent_ui.dart' show TreeViewItem;
 
-const String blank = '''
+const String _blank = '''
 <?xml version="1.0" encoding="UTF-8"?>
 <Authority xmlns="http://www.records.nsw.gov.au/schemas/RDA">
 	<Term itemno="1.0.0" type="function">
