8e8872f18db300221bc16971a250eb3c418beaed
Richard Lehane <richard.lehane@gmail.com>
2025-07-15 10:39:56 +1000

    Update xml_web.dart

--- lib/src/xml_web.dart
+++ lib/src/xml_web.dart
@@ -1,1 +1,20 @@
+import 'package:xml/xml.dart' show XmlElement;
 
+class Session {
+  List<XmlDocument>? documents;
+
+  int addString(String content) {
+    documents.add(XmlDocument.parse(content));
+    return documents.len - 1;
+  }
+
+  int addFile(PlatformFile f) {
+    if (f.bytes == null) {
+      //return DocumentModel.empty(title: f.name);
+      return documents.len - 1;
+    }
+    final doc = XmlDocument.parse(String.fromCharCodes(f.bytes!));
+    documents.add(doc);
+    return documents.len - 1;
+  }
+}
