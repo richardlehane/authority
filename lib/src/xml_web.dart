a88960cd210d955e4f36ed7ff7c117f7982a01e9
Richard Lehane <richard.lehane@gmail.com>
2025-07-15 11:59:48 +1000

    Update xml_web.dart

--- lib/src/xml_web.dart
+++ lib/src/xml_web.dart
@@ -1,14 +1,25 @@
 import 'package:xml/xml.dart' show XmlElement;
 
+const String _template = '''
+<?xml version="1.0" encoding="UTF-8"?>
+<Authority xmlns="http://www.records.nsw.gov.au/schemas/RDA">
+	<Term itemno="1.0.0" type="function">
+    <Term itemno="1.1.0" type="activity">
+      <Class itemno="1.1.1" />
+		</Term>
+	</Term>
+</Authority>
+''';
+
 class Session {
   List<XmlDocument>? documents;
 
-  int addString(String content) {
-    documents.add(XmlDocument.parse(content));
+  int empty() {
+    documents.add(XmlDocument.parse(_template));
     return documents.len - 1;
   }
 
-  int addFile(PlatformFile f) {
+  int load(PlatformFile f) {
     if (f.bytes == null) {
       //return DocumentModel.empty(title: f.name);
       return documents.len - 1;
