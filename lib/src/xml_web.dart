d4134466f758d261d39f5b7920ebc81e887ca296
Richard Lehane <richard.lehane@gmail.com>
2025-07-22 14:16:31 +1000

    Update xml_web.dart

--- lib/src/xml_web.dart
+++ lib/src/xml_web.dart
@@ -59,7 +59,76 @@ class Session {
     return "";
   }
 
-  
+  String getAttribute(int index, String name) {
+    XmlElement? el = nodes[int];
+    if (el == null) return "";
+    String? a = el!.getAttribute(
+      name,
+      namespace: "http://www.records.nsw.gov.au/schemas/RDA",
+    );
+    return (a != null) ? a : "";
+  }
+
+  void setElement(int index, String name, String value) {
+    XmlElement? el = nodes[int];
+    if (el == null) return;
+    XmlElement? t = el!.getElement(name);
+    // delete
+    if (value == "") {
+      if (t != null) el!.children.remove(t);
+      return;
+    }
+    // update
+    if (t != null) {
+      t.innerText = value;
+      return;
+    }
+    // insert
+    t = XmlElement(XmlName(name), [], [XmlText(value)], false);
+    int p = insertPos(el!, typ, name);
+    el!.children.insert(p, t);
+    return;
+  }
+
+  void setAttribute(int index, String name, String value) {
+    XmlElement? el = nodes[int];
+    if (el == null) return;
+    String? a = (value == "") ? null : value;
+    el!.setAttribute(
+      name,
+      a,
+      namespace: "http://www.records.nsw.gov.au/schemas/RDA",
+    );
+    return;
+  }
+
+  List<XmlElement>? getParagraphs(int index, String name) {
+    XmlElement? el = nodes[int];
+    if (el == null) return null;
+    XmlElement? parent = el!.getElement(name);
+    if (parent == null) return null;
+    return parent.findElements("Paragraph").toList();
+  }
+
+  void setParagraphs(int index, String name, List<XmlElement> paras) {
+    XmlElement? el = nodes[int];
+    if (el == null) return;
+    XmlElement? parent = el!.getElement(name);
+    if (parent == null) {
+      // inserting
+      parent = XmlElement(XmlName(name), [], paras, false);
+      int p = insertPos(el!, typ, name);
+      el!.children.insert(p, parent);
+      return;
+    }
+    parent.children.removeWhere(
+      (para) =>
+          para.nodeType == XmlNodeType.ELEMENT &&
+          (para as XmlElement).name.local == "Paragraph",
+    );
+    parent.children.insertAll(0, paras);
+  }
+	
   // multi operations
 	
 }
@@ -131,4 +200,44 @@ XmlElement? nth(XmlDocument? doc, int n) {
     return null;
   }
   return descend(doc.rootElement);
+}
+
+// Constants to maintain element order
+// Also, after Comments Term or Class elements can be nested
+const termElements = [
+  "ID",
+  "TermTitle",
+  "TermDescription",
+  "DateRange",
+  "Status",
+  "LinkedTo", // multiple
+  "Comment", // multiple
+];
+
+const classElements = [
+  "ID",
+  "ClassTitle",
+  "ClassDescription",
+  "Disposal", // multiple
+  "Justification",
+  "DateRange",
+  "Status",
+  "LinkedTo", // multiple
+  "Comment", // multiple
+];
+
+// determine where to insert a new element
+int insertPos(XmlElement el, NodeType typ, String name) {
+  int pos = 0;
+  List<String> prev = (typ == NodeType.termType) ? termElements : classElements;
+  prev = prev.sublist(0, prev.indexOf(name));
+  for (var n in el.children) {
+    if (n.nodeType != XmlNodeType.ELEMENT ||
+        prev.contains((n as XmlElement).name.local)) {
+      pos++;
+      continue;
+    }
+    return pos;
+  }
+  return pos;
 }
