93121a4b8a03bd672b3a8b0179a46bd46848bfb0
Richard Lehane <richard.lehane@gmail.com>
2025-07-22 14:25:45 +1000

    Update authority.dart

--- lib/src/authority.dart
+++ lib/src/authority.dart
@@ -1,6 +1,5 @@
-import 'authority_none.dart' // Stub implementation
-    if (dart.library.io) 'authority_win.dart' // dart:io implementation
-    if (dart.library.js_interop) 'authority_web.dart'; // package:web implementation
+import 'xml_web.dart' 
+    if (dart.library.io) 'xml_native.dart'; // dart:io implementation
 import 'package:xml/xml.dart' show XmlElement;
 import 'package:fluent_ui/fluent_ui.dart' show TreeViewItem;
 
