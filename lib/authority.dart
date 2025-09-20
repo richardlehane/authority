66e1699857486b2bc9cac6ee9be81627667b9855
Richard Lehane <richard@itforarchivists.com>
2025-09-20 20:51:59 +1000

    exports

--- lib/authority.dart
+++ lib/authority.dart
@@ -1,3 +1,4 @@
 export 'src/document.dart' show View, Document;
-export 'src/node.dart' show NodeType, CurrentNode;
+export 'src/node.dart'
+    show NodeType, CurrentNode, SeeRefType, StatusType, StatusKind;
 export 'src/tree.dart' show Ref, TreeNode, makeItem;
